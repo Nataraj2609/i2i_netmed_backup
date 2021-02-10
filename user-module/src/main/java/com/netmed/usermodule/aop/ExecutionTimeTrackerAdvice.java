03dfdb636a4a1049d0ddeaaa4531493dd8d61cb4
The_Tamil_Developer <36486498+Nataraj2609@users.noreply.github.com>
2021-02-10 12:22:32 +0530

    AOP Added successfully

---
+++ user-module/src/main/java/com/netmed/usermodule/aop/ExecutionTimeTrackerAdvice.java
@@ -0,0 +1,36 @@
+package com.netmed.usermodule.aop;
+
+import org.aspectj.lang.ProceedingJoinPoint;
+import org.aspectj.lang.annotation.Around;
+import org.aspectj.lang.annotation.Aspect;
+import org.aspectj.lang.reflect.MethodSignature;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Component;
+import org.springframework.util.StopWatch;
+
+@Aspect
+@Component
+public class ExecutionTimeTrackerAdvice {
+    public static final Logger logger = LoggerFactory.getLogger(ExecutionTimeTrackerAdvice.class);
+
+    @Around("@annotation(com.netmed.usermodule.aop.LogExecutionTime)")
+    public Object trackTime(ProceedingJoinPoint proceedingJoinPoint) throws Throwable {
+        MethodSignature methodSignature = (MethodSignature) proceedingJoinPoint.getSignature();
+
+        String className = methodSignature.getDeclaringType().getSimpleName();
+        String methodName = methodSignature.getName();
+
+        StopWatch stopWatch = new StopWatch(className + "->" + methodName);
+        stopWatch.start(methodName);
+        Object result = proceedingJoinPoint.proceed();
+        stopWatch.stop();
+
+        if (logger.isInfoEnabled()) {
+            logger.info(String.valueOf(stopWatch.getTotalTimeSeconds()));
+            logger.info(stopWatch.prettyPrint());
+        }
+
+        return result;
+    }
+}
--- user-module/src/main/java/com/netmed/usermodule/controller/UserController.java
+++ user-module/src/main/java/com/netmed/usermodule/controller/UserController.java
@@ -1,5 +1,6 @@
 package com.netmed.usermodule.controller;
 
+import com.netmed.usermodule.aop.LogExecutionTime;
 import com.netmed.usermodule.dto.UserDto;
 import com.netmed.usermodule.service.UserService;
 import io.swagger.annotations.ApiOperation;
@@ -33,6 +34,7 @@ public class UserController {
     @PostMapping
     @ResponseStatus(value = HttpStatus.CREATED)
     @ApiOperation(value = "Api Endpoint to create the User details")
+    @LogExecutionTime
     public UserDto createUser(@Valid @RequestBody UserDto userDto) {
         return userService.createUser(userDto);
     }
@@ -45,6 +47,7 @@ public class UserController {
      */
     @GetMapping(path = "/{userId}")
     @ApiOperation(value = "Api Endpoint to get the User details")
+    @LogExecutionTime
     public UserDto getUser(@PathVariable long userId) {
         return userService.getUser(userId);
     }
@@ -57,6 +60,7 @@ public class UserController {
      */
     @PutMapping
     @ApiOperation(value = "Api Endpoint to update the User details - Password & Role can be only updated")
+    @LogExecutionTime
     public UserDto updateUser(@Valid @RequestBody UserDto userDto) {
         return userService.updateUser(userDto);
     }
@@ -70,6 +74,7 @@ public class UserController {
     @DeleteMapping(path = "/{userId}")
     @ResponseStatus(value = HttpStatus.NO_CONTENT)
     @ApiOperation(value = "Api Endpoint to delete the User details")
+    @LogExecutionTime
     public void deleteUser(@PathVariable long userId) {
         userService.deleteUser(userId);
     }
@@ -84,6 +89,7 @@ public class UserController {
      */
     @GetMapping("getallusers")
     @ApiOperation(value = "Api Endpoint to retrieve all the User details")
+    @LogExecutionTime
     public List<UserDto> getAllUsers(@RequestParam(name = "page", defaultValue = "0") int page,
                                      @RequestParam(name = "limit", defaultValue = "10") int limit,
                                      @RequestParam(name = "orderBy", defaultValue = "asc") String orderBy) {
@@ -101,6 +107,7 @@ public class UserController {
      */
     @GetMapping("searchuser")
     @ApiOperation(value = "Api Endpoint to search for the User - Only using User name")
+    @LogExecutionTime
     public List<UserDto> searchUser(@RequestParam(name = "search") String search,
                                     @RequestParam(name = "page", defaultValue = "0") int page,
                                     @RequestParam(name = "limit", defaultValue = "10") int limit,
@@ -116,6 +123,7 @@ public class UserController {
      */
     @GetMapping("search")
     @ApiOperation(value = "Api Endpoint to search for the User - Only using User name - using Like")
+    @LogExecutionTime
     public List<UserDto> search(@RequestParam(name = "search") String search) {
         return userService.search(search);
     }
