890a4a5587ff267616e868df677b47cd53b2058b
The_Tamil_Developer <36486498+Nataraj2609@users.noreply.github.com>
2021-03-09 19:10:09 +0530

    Rabbitmq optimised code 3

--- patient-module/src/main/java/com/netmed/patientmodule/PatientModuleApplication.java
+++ patient-module/src/main/java/com/netmed/patientmodule/PatientModuleApplication.java
@@ -1,5 +1,6 @@
 package com.netmed.patientmodule;
 
+import org.springframework.amqp.rabbit.annotation.EnableRabbit;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cache.annotation.EnableCaching;
--- patient-module/src/main/java/com/netmed/patientmodule/config/RabbitMqConfig.java
+++ patient-module/src/main/java/com/netmed/patientmodule/config/RabbitMqConfig.java
@@ -1,47 +1,20 @@
 package com.netmed.patientmodule.config;
 
-import org.springframework.amqp.core.*;
-import org.springframework.amqp.rabbit.connection.ConnectionFactory;
-import org.springframework.amqp.rabbit.core.RabbitTemplate;
 import org.springframework.amqp.support.converter.Jackson2JsonMessageConverter;
 import org.springframework.amqp.support.converter.MessageConverter;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 /***
- * Configuration class for RabbitMq middlewareSS
+ * Configuration class for RabbitMq middleware - Setting JsonMessageConverter(Serializer)
  */
 @Configuration
 public class RabbitMqConfig {
 
     public static final String QUEUE_NAME = "new_user_notify_queue";
-    public static final String EXCHANGE_NAME = "new_user_notify_exchange";
-    public static final String ROUTING_KEY = "new_user_routing_key";
-
-    @Bean
-    public Queue queue() {
-        return new Queue(QUEUE_NAME);
-    }
-
-    @Bean
-    public TopicExchange topicExchange() {
-        return new TopicExchange(EXCHANGE_NAME);
-    }
-
-    @Bean
-    public Binding binding(Queue queue, TopicExchange topicExchange) {
-        return BindingBuilder.bind(queue).to(topicExchange).with(ROUTING_KEY);
-    }
 
     @Bean
     public MessageConverter jsonMessageConverter() {
         return new Jackson2JsonMessageConverter();
-    }
-
-    @Bean
-    public AmqpTemplate template(ConnectionFactory connectionFactory) {
-        final RabbitTemplate rabbitTemplate = new RabbitTemplate(connectionFactory);
-        rabbitTemplate.setMessageConverter(jsonMessageConverter());
-        return rabbitTemplate;
     }
 }
